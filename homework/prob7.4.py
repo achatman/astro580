43b07b4f93fbcd1f51f882adec6203722e85c43f
achatman <andrew.chatman9@gmail.com>
2018-03-20 16:44:59 -0500

    Finished problem 7.4

--- homework/prob7.4.py
+++ homework/prob7.4.py
@@ -3,26 +3,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-if len(sys.argv) != 2:
-    print('usage: python prob7.4.py n')
-
-n = int(sys.argv[1])
+n = 2
 
 def lane_emden(x, Y):
-    y = Y[1]
-    z = -Y[0]**n - (2/x)*Y[1]
-    return y,z
+    global n
+    y, z = Y
+
+    dydx = z
+    if x == 0:
+        dzdx = -y**n + (2/3)
+    else:
+        dzdx = -y**n -  (2/x)*z
+
+    return dydx, dzdx
 
 end_xi = 15
-points = np.linspace(1, 15, 10000)
-r = solve_ivp(lane_emden, (1, end_xi), (1, 0), t_eval = points)
+points = np.linspace(0, end_xi, 1000)
+
+
 
 plt.style.use('seaborn')
 linestyles = ['-', '--']
 plt.figure(figsize=(10,16))
-[plt.plot(r.t, y, linestyle=ls) for (y, ls) in zip(r.y, linestyles)]
-labels = [r'y', r'z']
-plt.legend(labels, loc=1)
-plt.xlabel('x')
-plt.ylabel('y and z')
-plt.show()
+for n in np.linspace(2, 4, 7):
+    r = solve_ivp(lane_emden, (0, end_xi), (1, 0), t_eval = points)
+    mask = (r.y[0] >= 0) & (r.y[1] <= 1)
+    x = r.t[mask]
+    y = r.y[0][mask]
+    z = r.y[1][mask]
+    plt.subplot(211)
+    plt.plot(x, y, label=f'n={n:.1f}')
+    plt.subplot(212)
+    plt.plot(x, z, label=f'n={n:.1f}')
+plt.subplot(211)
+plt.xlabel(r'$x=\xi$')
+plt.ylabel(r'$y=\theta_n$')
+plt.subplot(212)
+plt.xlabel(r'$x=\xi$')
+plt.ylabel(r'$z=\frac{d\theta_n}{d\xi}$')
+plt.legend(loc = 4)
+plt.suptitle(r'y and z vs x for a range of n $\in$ [2,4]')
+plt.savefig('prob7.4.pdf', format='pdf')
